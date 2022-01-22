e05a968e67efb3aa05e604bc350d2a6ba7e1a10f
DenisStabronik <94472990+DenisStabronik@users.noreply.github.com>
2022-01-22 20:12:23 +0300

    Переписал программы с 0
    В Diretory_struct.py ,в качестве разделителя между папками нужно использовать прямой слеш "/",  объект Path конвертирует слеш, согласно операционной системе, если оставить обратный слеш "\", то программа будет работать только на Windows (в Windows используется \ , в Linux / для разделения пути к папкам-файлам)

--- 5/Diretory_struct.py
+++ 5/Diretory_struct.py
@@ -2,22 +2,16 @@ from pathlib import Path
 import re
 
 
-def restructure_directory(directory: str) -> None:
+def change_name_on_dir(directory: str):
     path_start = './' + directory + '/'
     path = Path('./' + directory + '/')
-    for child in path.iterdir():
-        if not child.is_dir():
-            old_file_name = child.stem
-            old_file_extension = child.suffix
-
-            old_path = old_file_name + old_file_extension
-            full_path = re.sub('-', r"\\", old_path)
-            new_file_name = re.search(r"\d+\.txt", full_path).group(0)
-            path_to_new_file = re.search(r"\d{4}\\\d+\\", full_path).group(0)
+    for currentFile in path.iterdir():             
+        if currentFile is not dir:
+            file_name = currentFile.name
+            full_path = re.sub('-', r"/", file_name)
+            new_file_name = re.search(r"\d+.txt", full_path).group(0)
+            path_to_new_file = re.search(r"\d{4}/\d+/", full_path).group(0)
             Path(path_start + path_to_new_file).mkdir(parents=True, exist_ok=True)
             Path(path_start + path_to_new_file + new_file_name).touch()
+            currentFile.unlink()
 
-            info_file = child.read_text()
-            Path(path_start + path_to_new_file + new_file_name).write_text(info_file)
-
-            child.unlink()
--- 5/main1.py
+++ 5/main1.py
@@ -1,41 +1,34 @@
 import argparse
-from Rest_Days import days_before_event
+from Rest_Days import days_before_new_year
 from Generator import generate_sample
 from Toy import get_random_toy
-from Diretory_struct import restructure_directory as rest_dir
+from Diretory_struct import change_name_on_dir 
 import logging
-
-logging.basicConfig(filename="sample.log", level=logging.INFO, filemode="w")
-
+logging.basicConfig(
+    level=logging.INFO, 
+    filename = "mylog.log", 
+    format = "%(asctime)s - %(module)s - %(levelname)s - %(funcName)s: %(lineno)d - %(message)s", 
+    datefmt='%H:%M:%S',
+    )
+# logging.basicConfig(filename="logfile.log", level=logging.INFO, filemode="w")
 parser = argparse.ArgumentParser()
-sub_parser = parser.add_subparsers(dest="command")
 
-parser_newyear = sub_parser.add_parser("newyear")
-parser_newyear.add_argument('--hours', help='Additional parameter,add hours to the days before the event ',
-                            action='store_true')
+parser.add_argument('-a', '--action', help='what function do you want to run?')
+parser.add_argument('-d', '--directory', help='specify a directory')
 
-parser_toy = sub_parser.add_parser("toy")
-
-parser_restructure = sub_parser.add_parser("restructure")
-parser_restructure.add_argument("directory", type=str)
 args = parser.parse_args()
 
-if args.command == "newyear":
-    hour_flag = args.hours
-    if hour_flag:
-        logging.info("Received newyear argument with hours")
-    else:
-        logging.info("Received newyear argument without hours")
-    days_before_event(hour_flag)
-    logging.info("Calculated days before event")
-if args.command == "toy":
-    logging.info("Received toy argument")
+if args.action == "NY":
+    days_before_new_year()
+    logging.info("Calculated days before 2023")
+elif args.action == "Toy":
     get_random_toy()
-    logging.info("Generate random toy")
-if args.command == "restructure":
+    logging.info("Get random toy")
+elif args.action == "R":
     directory = args.directory
-    logging.info(f"Received restructure argument with directory = {directory}")
+    logging.info('Enter Restructure argument and directory argument')
     generate_sample(directory)
-    logging.info("Generate samples")
-    rest_dir(directory)
-    logging.info("Restructure files")
+    logging.info("Generate files")   
+    change_name_on_dir (directory)
+logging.info("Restructure files")
+# print(args)
